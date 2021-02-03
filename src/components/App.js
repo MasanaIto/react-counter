aad78dc41038d26cf2150075a6ec1507b29627fc
MasanaIto <z3heng0326@gmail.com>
2021-02-03 18:00:12 +0900

    文字変更機能、カウンター機能実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react"
 
-const App = () =>{
+const App = (props) =>{
+  const [count, setCount] = useState(props.count)
+  const [message, setMessage] = useState(props.message)
+
   return (
     <>
-      hello
+      <p>
+        {" "}
+        {message}:{count}{" "}
+      </p>
+      <button onClick={() => setCount((cnt) => ++cnt)}> + </button>
+      <button onClick={() => setCount((cnt) => --cnt)}> - </button>
+      <div>
+        message :{" "}
+        <input 
+          type="text"
+          value={message}
+          onChange={(e) => setMessage((msg) => (msg = e.target.value))}
+        />
+      </div>
     </>
   )
+}
+App.defaultProps = {
+  message: "hello",
+  count: 0,
 }
 
 export default App
